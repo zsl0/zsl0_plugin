c2e7787402497a98f7af90abcf19978ab1bec525
zsl <603405203@qq.com>
2022-06-05 11:11:43 +0800

    fix(AccessLogInterceptor): 移除无用注释代码

--- common/zsl-spring-boot-starter-log/src/main/java/com/zsl/custombox/log/core/interceptor/AccessLogInterceptor.java
+++ common/zsl-spring-boot-starter-log/src/main/java/com/zsl/custombox/log/core/interceptor/AccessLogInterceptor.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 /**
  * 访问记录(系统日志)
- *  todo 实现@LogRecord注解实现操作日志
+ *  实现@LogRecord注解实现操作日志
  *
  * @Author zsl
  * @Date 2022/5/22 14:55
@@ -40,26 +40,6 @@ public class AccessLogInterceptor implements HandlerInterceptor {
                 .setStartTime(new Date(System.currentTimeMillis()));
         // 存储全局日志记录上下文
         SystemLogContextHolder.set(systemLogContext);
-/*        // format log
-        StringBuilder requestStr = new StringBuilder();
-        List<Object> requestArgs = new ArrayList<>();
-        requestStr.append("\n=========================== Request Start ===========================\n");
-        requestStr.append(String.format("       %-10s: {}\n", "userId"));
-        requestArgs.add(logRecordContext.getUserId());
-        requestStr.append(String.format("       %-10s: {}\n", "requestNo"));
-        requestArgs.add(logRecordContext.getRequestNo());
-        requestStr.append(String.format("       %-10s: {}\n", "ip"));
-        requestArgs.add(logRecordContext.getIp());
-        requestStr.append(String.format("       %-10s: {}\n", "uri"));
-        requestArgs.add(logRecordContext.getUri());
-        requestStr.append(String.format("       %-10s: {}\n", "param"));
-        requestArgs.add(logRecordContext.getParam());
-        requestStr.append(String.format("       %-10s: {}\n", "method"));
-        requestArgs.add(logRecordContext.getMethod());
-        requestStr.append(String.format("       %-10s: {}\n", "startTime"));
-        requestArgs.add(logRecordContext.getStartTime());
-        requestStr.append("=========================== Request End ===========================\n");
-        log.info(requestStr.toString(), requestArgs.toArray());*/
         return true;
     }
 
@@ -95,8 +75,6 @@ public class AccessLogInterceptor implements HandlerInterceptor {
         requestArgs.add(systemLogContext.getRespCode());
         requestStr.append(String.format("       %-10s: {}\n", "respMsg"));
         requestArgs.add(systemLogContext.getRespMsg());
-//        requestStr.append(String.format("       %-10s: {}\n", "respBody"));
-//        requestArgs.add(systemLogContext.getRespBody());
         requestStr.append("=================================================================\n");
 
         // todo 日志入库
